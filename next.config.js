f25d2cec8648a2fb3156e77fa640bb47246df4cc
KingRay171 <raycikome@gmail.com>
2023-06-14 13:39:42 -0400

    fixed env vars

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  env: {
-    GOOGLE_API_KEY : "AIzaSyB2GR4U-_tdTrHrM5qsLe2S8ZZaP5SNf2A",
-  },
-}
+module.exports = nextConfig
