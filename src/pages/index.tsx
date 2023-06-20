a47b07da3e35d1937865b1f8b31cdab6a303eea2
Ray Ikome <raycikome@gmail.com>
2023-06-19 23:11:43 -0400

    added map, markers, prices, and integrated distance api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,88 @@
-import React, {useState, useCallback, forwardRef, useEffect} from 'react';
-import {GoogleMapsProvider} from '@ubilabs/google-maps-react-hooks';
-import GoogleMapReact from "google-map-react"
-import Nav from './navbar';
 
-function App() {
-  const [val, setVal] = useState({lat: 10, lng: 10});
-  const [stationLocs, setStationLocs] = useState({})
+import { useLoadScript, GoogleMap, MarkerF, useGoogleMap } from '@react-google-maps/api';
+import type { NextPage } from 'next';
+import { useMemo, useState, useEffect, useRef } from 'react';
+import Nav from './navbar';
+import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText } from '@mui/material';
+
+const Home: NextPage = () => {
+  const libraries = useMemo(() => ['places'], []);
+  const [val, setVal] = useState({lat: 0, lng: 0})
+  const [stations, setStations] = useState({primaryStations: [{id: 0, lat: 0, lng: 0, price: "0"}], secondaryStations: [{id: 0, lat: 0, lng: 0, price: "0"}]})
   const [e, setE] = useState(0)
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [currentStation, setCurrentStation] = useState({id: 0, lat: 0, lng: 0, price: "0", distance: "0"})
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((pos) => {
         setVal({lat: pos.coords.latitude, lng: pos.coords.longitude})
+        fetch("/api/hello", {method: "POST", body: JSON.stringify({lat: pos.coords.latitude, lng: pos.coords.longitude})}).then((data) => data.json()).then((data) => setStations(data))
       })
     }
   }, [e])
 
-  const interval = setInterval(() => {
-    setE(e + 1)
-  }, 500)
 
-  const fn = () => {console.log("hi")}
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: true,
+      zoomControl: true,
+      
+    }),
+    [e]
+  );
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string,
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '90vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_API_KEY }}
+    <div >
+      <GoogleMap
+        options={mapOptions}
+        zoom={13}
         center={val}
-
-
-        defaultZoom={9}
-
-      >
-
-      </GoogleMapReact>
-    <Nav onClick={() => console.log("hi")} />
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerStyle={{ height: '90vh', width: '100%' }}
+        onLoad={() => console.log('Map Component Loaded...')}>
+          {stations.primaryStations.map((station) => (
+            <MarkerF key={station.id} position={{lat: station.lat, lng: station.lng}} label={station.price}/>
+          ))}
+          {stations.secondaryStations.map((station) => (
+            <MarkerF key={station.id} position={{lat: station.lat, lng: station.lng}} label={station.price} onClick={() => {
+              fetch(
+                `/api/directions`, 
+                {
+                  method: "POST", 
+                  body: JSON.stringify({currentLat: val.lat, currentLng: val.lng, destLat: station.lat, destLng: station.lng})
+                }
+              ).then(data => data.json()).then(data => {
+                console.log(data.rows[0].elements[0].distance.text)
+                setCurrentStation({id: 0, lat: 0, lng: 0, price: "0", distance: data.rows[0].elements[0].distance.text})
+                setDialogOpen(true)
+              })
+            }}/>
+          ))}
+          <MarkerF position={val} />
+          <Dialog open={dialogOpen} onClose={() => {setDialogOpen(false)}}>
+            <DialogTitle>Station</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                {currentStation.distance}
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
+      </GoogleMap>
+      <Nav onClick={() => setE(e + 1)} />
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
